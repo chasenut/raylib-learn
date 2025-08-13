19df1f4435834e10eeaceb21eb4605284777035f
chasenut <switalamaciej5@gmail.com>
2025-08-13 17:20:36 +0200

    some starting stuff

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/chasenut/raylib-learn
 
 go 1.24.5
+
+require (
+	github.com/ebitengine/purego v0.8.4 // indirect
+	github.com/gen2brain/raylib-go/raylib v0.55.1 // indirect
+	golang.org/x/exp v0.0.0-20250811191247-51f88131bc50 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -1,9 +1,57 @@
 package main
 
 import (
-	"fmt"
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+const (
+	screenWidth int32 = 1000
+	screenHeight int32 = 480
+)
+
+var (
+	running bool = true
+)
+
+func drawScene() {
+
+}
+
+func input() {
+
+}
+
+func update() {
+	running = !rl.WindowShouldClose()
+	
+}
+
+func render() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+		drawScene()
+
+		rl.EndDrawing()
+}
+
+func init() {
+	rl.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window")
+	rl.SetExitKey(0)
+	rl.SetTargetFPS(60)
+
+}
+
+func quit() {
+	defer rl.CloseWindow()
+
+}
+
 func main() {
-	fmt.Println("hello go")
+	for running {
+		input()
+		update()
+		render()
+	}
+	quit()
 }
